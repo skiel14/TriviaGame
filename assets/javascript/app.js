cb95623dda35f39fae8d3101bc9456e20f0f0194
skiel <platypony14@gmail.com>
2019-03-23 21:50:02 -0400

    still css to do

---
+++ assets/javascript/app.js
@@ -0,0 +1,706 @@
+var questions = {
+  "easy": [
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Who was the only god from Greece who did not get a name change in Rome?",
+    "correct_answer": "Apollo",
+    "incorrect_answers": [
+    "Demeter",
+    "Zeus",
+    "Athena"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "The ancient roman god of war was commonly known as...",
+    "correct_answer": "Mars",
+    "incorrect_answers": [
+    "Jupiter",
+    "Juno",
+    "Ares"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Who in Greek mythology, who led the Argonauts in search of the Golden Fleece?",
+    "correct_answer": "Jason",
+    "incorrect_answers": [
+    "Castor",
+    "Daedalus",
+    "Odysseus"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "This Greek goddess&#039;s name was chosen for the dwarf planet responsible for discord on Pluto&#039;s classification amongst astronomers.",
+    "correct_answer": "Eris",
+    "incorrect_answers": [
+    "Charon",
+    "Ceres",
+    "Dysnomia"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Who was the King of Gods in Ancient Greek mythology?",
+    "correct_answer": "Zeus",
+    "incorrect_answers": [
+    "Apollo",
+    "Hermes",
+    "Poseidon"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which Greek &amp; Roman god was known as the god of music, truth and prophecy, healing, the sun and light, plague, poetry, and more?",
+    "correct_answer": "Apollo",
+    "incorrect_answers": [
+    "Aphrodite",
+    "Artemis",
+    "Athena"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "The greek god Poseidon was the god of what?",
+    "correct_answer": "The Sea",
+    "incorrect_answers": [
+    "War",
+    "Sun",
+    "Fire"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which figure from Greek mythology traveled to the underworld to return his wife Eurydice to the land of the living?",
+    "correct_answer": "Orpheus",
+    "incorrect_answers": [
+    "Hercules",
+    "Perseus",
+    "Daedalus"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "In Greek mythology, who is the god of wine?",
+    "correct_answer": "Dionysus",
+    "incorrect_answers": [
+    "Hephaestus",
+    "Demeter",
+    "Apollo"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "In most traditions, who was the wife of Zeus?",
+    "correct_answer": "Hera",
+    "incorrect_answers": [
+    "Aphrodite",
+    "Athena",
+    "Hestia"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "Which of these mythological creatures is said to be half-man and half-horse?",
+    "correct_answer": "Centaur",
+    "incorrect_answers": [
+    "Minotaur",
+    "Pegasus",
+    "Gorgon"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "easy",
+    "question": "What mythology did the god &quot;Apollo&quot; come from?",
+    "correct_answer": "Greek and Roman",
+    "incorrect_answers": [
+    "Roman and Spanish",
+    "Greek and Chinese",
+    "Greek, Roman and Norse"
+    ]
+    }
+  ],
+  "medium": [
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "According to Algonquian folklore, how does one transform into a Wendigo?",
+    "correct_answer": "Participating in cannibalism.",
+    "incorrect_answers": [
+    "Excessive mutilation of animal corpses.",
+    "Performing a ritual involving murder.",
+    "Drinking the blood of many slain animals."
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "This Greek mythological figure is the god/goddess of battle strategy (among other things).",
+    "correct_answer": "Athena",
+    "incorrect_answers": [
+    "Ares",
+    "Artemis",
+    "Apollo"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "The Hippogriff, not to be confused with the Griffon, is a magical creature with the front half of an eagle, and the back half of what?",
+    "correct_answer": "A Horse",
+    "incorrect_answers": [
+    "A Dragon",
+    "A Tiger",
+    "A Lion"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Who was the Roman god of fire?",
+    "correct_answer": "Vulcan",
+    "incorrect_answers": [
+    "Apollo",
+    "Jupiter",
+    "Mercury"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Neptune&#039;s greek name was...",
+    "correct_answer": "Poseidon",
+    "incorrect_answers": [
+    "Ares",
+    "Zeus",
+    "Apollo"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Hera is god of...",
+    "correct_answer": "Marriage",
+    "incorrect_answers": [
+    "Agriculture",
+    "Sea",
+    "War"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Which of the following Mesopotamian mythological figures was NOT a deity?",
+    "correct_answer": "Enkidu",
+    "incorrect_answers": [
+    "Enki",
+    "Enlil",
+    "Enkimdu"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Which of these Roman gods doesn&#039;t have a counterpart in Greek mythology?",
+    "correct_answer": "Janus",
+    "incorrect_answers": [
+    "Vulcan",
+    "Juno",
+    "Mars"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "In Greek Mythology, who killed Achilles?",
+    "correct_answer": "Paris",
+    "incorrect_answers": [
+    "Hector",
+    "Helen",
+    "Pericles"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "A minotaur is half human half what?",
+    "correct_answer": "Bull",
+    "incorrect_answers": [
+    "Cow",
+    "Horse",
+    "Eagle"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "What is the name of the Greek god of blacksmiths?",
+    "correct_answer": "Hephaestus",
+    "incorrect_answers": [
+    "Dyntos",
+    "Vulcan",
+    "Artagatus"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "According to the Egyptian Myth of Osiris, who murdered Osiris?",
+    "correct_answer": "Set",
+    "incorrect_answers": [
+    "Horus",
+    "Ra",
+    "Anhur"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Which of the following is not one of the Greek Fates?",
+    "correct_answer": "Narcissus",
+    "incorrect_answers": [
+    "Clotho",
+    "Atropos",
+    "Lachesis"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "In African mythology, Anansi is a trickster and storyteller who takes the shape of which animal?",
+    "correct_answer": "Spider",
+    "incorrect_answers": [
+    "Wild dog",
+    "Monkey",
+    "Crocodile"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "The Maori hold that which island nation was founded by Kupe, who discovered it under a long white cloud?",
+    "correct_answer": "New Zealand",
+    "incorrect_answers": [
+    "Vanuatu",
+    "Fiji",
+    "Hawaii"
+    ]
+    }
+  ],
+  "hard": [
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Talos, the mythical giant bronze man, was the protector of which island?",
+    "correct_answer": "Crete",
+    "incorrect_answers": [
+    "Sardinia",
+    "Sicily",
+    "Cyprus"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What was the punishment for Sysiphus&#039;s craftiness?",
+    "correct_answer": "Cursed to roll a boulder up a hill for eternity.",
+    "incorrect_answers": [
+    "Tied to a boulder for eternity, being pecked by birds.",
+    "Standing in a lake filled with water he could not drink.",
+    "To fell a tree that regenerated after every axe swing."
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "In Greek Mythology, who was the daughter of King Minos?",
+    "correct_answer": "Ariadne",
+    "incorrect_answers": [
+    "Athena",
+    "Ariel",
+    "Alana"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "In Norse mythology, what is the name of the serpent which eats the roots of the ash tree Yggdrasil?",
+    "correct_answer": "Nidhogg",
+    "incorrect_answers": [
+    "Bragi",
+    "Odin",
+    "Ymir"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which of the following is NOT a god in Norse Mythology.",
+    "correct_answer": "Jens",
+    "incorrect_answers": [
+    "Loki",
+    "Tyr",
+    "Snotra"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Nidhogg is a mythical creature from what mythology?",
+    "correct_answer": "Norse",
+    "incorrect_answers": [
+    "Egyptian",
+    "Greek",
+    "Hindu"
+    ]
+    },
+    {
+    "category": "Mythology",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Who is the Egyptian god of reproduction and lettuce?",
+    "correct_answer": "Min",
+    "incorrect_answers": [
+    "Menu",
+    "Mut",
+    "Meret"
+    ]
+    }
+  ]
+}
+$("#game").hide();
+$("#endgame").hide();
+var qNum=0, form=0, correct=0, incorrect=0, difficulty=1;
+
+$("#newGame").on("click", function() {
+  qNum=0;
+  correct=0;
+  incorrect=0;
+  $("#game").show();
+  setQuestion();
+});
+
+$("#easy").on("click", function() {
+  difficulty=1;
+  qNum=0;
+  correct=0;
+  incorrect=0;
+  $("#game").show();
+  setQuestion();
+});
+$("#medium").on("click", function() {
+  difficulty=2;
+  qNum=0;
+  correct=0;
+  incorrect=0;
+  $("#game").show();
+  setQuestion();
+});
+$("#hard").on("click", function() {
+  difficulty=3;
+  qNum=0;
+  correct=0;
+  incorrect=0;
+  $("#game").show();
+  setQuestion();
+});
+
+var answered = false;
+$(".answer").click(function() {
+  var opt = ($(this).attr("value"));
+  if(form==0) {
+    if (opt=="1") {
+      correct++;
+    } else {
+      incorrect++;
+      showIncorrect(opt);
+    }
+  } else if(form==1) {
+    if (opt=="2") {
+      correct++;
+    } else {
+      incorrect++;
+      showIncorrect(opt);
+    }
+  } else if(form==2) {
+    if (opt=="3") {
+      correct++;
+    } else {
+      incorrect++;
+      showIncorrect(opt);
+    }
+  } else if(form==3) {
+    if (opt=="4") {
+      correct++;
+    } else {
+      incorrect++;
+      showIncorrect(opt);
+    }
+  }
+  showCorrect();
+  answered = true;
+});
+
+function reset() {
+  $("#question").empty();
+  $("#opt1").empty();
+  $("#opt2").empty();
+  $("#opt3").empty();
+  $("#opt4").empty();
+
+  $("#ans1").addClass("bg-light");
+  $("#ans1").removeClass("text-white bg-success bg-danger");
+  $("#ans2").addClass("bg-light");
+  $("#ans2").removeClass("text-white bg-success bg-danger");
+  $("#ans3").addClass("bg-light");
+  $("#ans3").removeClass("text-white bg-success bg-danger");
+  $("#ans4").addClass("bg-light");
+  $("#ans4").removeClass("text-white bg-success bg-danger");
+}
+
+var finished=false;
+function setQuestion() {
+  if(difficulty==1) {
+    if(qNum >= questions.easy.length) {
+      finished = true;
+      $("#game").hide();
+      $("#correct").text(correct);
+      $("#incorrect").text(incorrect);
+      $("#endgame").show()
+    } else {
+      reset();
+      $("#question").text(questions.easy[qNum].question);
+
+      form = Math.floor(Math.random() * 4);
+
+      if (form==0) {
+        $("#opt1").text(questions.easy[qNum].correct_answer);
+        $("#opt2").text(questions.easy[qNum].incorrect_answers[2]);
+        $("#opt3").text(questions.easy[qNum].incorrect_answers[1]);
+        $("#opt4").text(questions.easy[qNum].incorrect_answers[0]);
+      } else if (form==1) {
+        $("#opt1").text(questions.easy[qNum].incorrect_answers[0]);
+        $("#opt2").text(questions.easy[qNum].correct_answer);
+        $("#opt3").text(questions.easy[qNum].incorrect_answers[2]);
+        $("#opt4").text(questions.easy[qNum].incorrect_answers[1]);
+      } else if (form==2) {
+        $("#opt1").text(questions.easy[qNum].incorrect_answers[1]);
+        $("#opt2").text(questions.easy[qNum].incorrect_answers[0]);
+        $("#opt3").text(questions.easy[qNum].correct_answer);
+        $("#opt4").text(questions.easy[qNum].incorrect_answers[2]);
+      } else if (form==3) {
+        $("#opt1").text(questions.easy[qNum].incorrect_answers[2]);
+        $("#opt2").text(questions.easy[qNum].incorrect_answers[1]);
+        $("#opt3").text(questions.easy[qNum].incorrect_answers[0]);
+        $("#opt4").text(questions.easy[qNum].correct_answer);
+      }
+      timer15();
+      qNum++;
+    }
+  } else if(difficulty==2) {
+    if(qNum >= questions.medium.length) {
+      finished = true;
+      $("#game").hide();
+      $("#correct").text(correct);
+      $("#incorrect").text(incorrect);
+      $("#endgame").show()
+    } else {
+      reset();
+      $("#question").text(questions.medium[qNum].question);
+
+      form = Math.floor(Math.random() * 4);
+
+      if (form==0) {
+        $("#opt1").text(questions.medium[qNum].correct_answer);
+        $("#opt2").text(questions.medium[qNum].incorrect_answers[2]);
+        $("#opt3").text(questions.medium[qNum].incorrect_answers[1]);
+        $("#opt4").text(questions.medium[qNum].incorrect_answers[0]);
+      } else if (form==1) {
+        $("#opt1").text(questions.medium[qNum].incorrect_answers[0]);
+        $("#opt2").text(questions.medium[qNum].correct_answer);
+        $("#opt3").text(questions.medium[qNum].incorrect_answers[2]);
+        $("#opt4").text(questions.medium[qNum].incorrect_answers[1]);
+      } else if (form==2) {
+        $("#opt1").text(questions.medium[qNum].incorrect_answers[1]);
+        $("#opt2").text(questions.medium[qNum].incorrect_answers[0]);
+        $("#opt3").text(questions.medium[qNum].correct_answer);
+        $("#opt4").text(questions.medium[qNum].incorrect_answers[2]);
+      } else if (form==3) {
+        $("#opt1").text(questions.medium[qNum].incorrect_answers[2]);
+        $("#opt2").text(questions.medium[qNum].incorrect_answers[1]);
+        $("#opt3").text(questions.medium[qNum].incorrect_answers[0]);
+        $("#opt4").text(questions.medium[qNum].correct_answer);
+      }
+      timer15();
+      qNum++;
+    }
+  } else if(difficulty==3) {
+    if(qNum >= questions.hard.length) {
+      finished = true;
+      $("#game").hide();
+      $("#correct").text(correct);
+      $("#incorrect").text(incorrect);
+      $("#endgame").show()
+    } else {
+      reset();
+      $("#question").text(questions.hard[qNum].question);
+
+      form = Math.floor(Math.random() * 4);
+
+      if (form==0) {
+        $("#opt1").text(questions.hard[qNum].correct_answer);
+        $("#opt2").text(questions.hard[qNum].incorrect_answers[2]);
+        $("#opt3").text(questions.hard[qNum].incorrect_answers[1]);
+        $("#opt4").text(questions.hard[qNum].incorrect_answers[0]);
+      } else if (form==1) {
+        $("#opt1").text(questions.hard[qNum].incorrect_answers[0]);
+        $("#opt2").text(questions.hard[qNum].correct_answer);
+        $("#opt3").text(questions.hard[qNum].incorrect_answers[2]);
+        $("#opt4").text(questions.hard[qNum].incorrect_answers[1]);
+      } else if (form==2) {
+        $("#opt1").text(questions.hard[qNum].incorrect_answers[1]);
+        $("#opt2").text(questions.hard[qNum].incorrect_answers[0]);
+        $("#opt3").text(questions.hard[qNum].correct_answer);
+        $("#opt4").text(questions.hard[qNum].incorrect_answers[2]);
+      } else if (form==3) {
+        $("#opt1").text(questions.hard[qNum].incorrect_answers[2]);
+        $("#opt2").text(questions.hard[qNum].incorrect_answers[1]);
+        $("#opt3").text(questions.hard[qNum].incorrect_answers[0]);
+        $("#opt4").text(questions.hard[qNum].correct_answer);
+      }
+      timer15();
+      qNum++;
+    }
+  }
+}
+
+function showIncorrect(clicked) {
+  if(clicked=="1") {
+    $("#ans1").removeClass("bg-light");
+    $("#ans1").addClass("text-white bg-danger");
+  } else if(clicked=="2") {
+    $("#ans2").removeClass("bg-light");
+    $("#ans2").addClass("text-white bg-danger");
+  } else if(clicked=="3") {
+    $("#ans3").removeClass("bg-light");
+    $("#ans3").addClass("text-white bg-danger");
+  } else if(clicked=="4") {
+    $("#ans4").removeClass("bg-light");
+    $("#ans4").addClass("text-white bg-danger");
+  }
+}
+
+function showCorrect() {
+  if(form==0) {
+    $("#ans1").removeClass("bg-light");
+    $("#ans1").addClass("text-white bg-success");
+  } else if(form==1) {
+    $("#ans2").removeClass("bg-light");
+    $("#ans2").addClass("text-white bg-success");
+  } else if(form==2) {
+    $("#ans3").removeClass("bg-light");
+    $("#ans3").addClass("text-white bg-success");
+  } else if(form==3) {
+    $("#ans4").removeClass("bg-light");
+    $("#ans4").addClass("text-white bg-success");
+  }
+}
+
+var intervalId;
+var number;
+
+function timer15() {
+  number=15;
+  function runTimer() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+  function decrement() {
+    number--;
+    $("#timer").text(number);
+    if(answered==true){
+      answered=false;
+      stopTimer();
+      $("#timer").empty();
+      timer3();
+    } else if (number == 0) {
+      alert("Times Up!");
+      incorrect++;
+      showCorrect();
+      stopTimer();
+      $("#timer").empty();
+      timer3();
+    }
+  }
+  function stopTimer() {
+    clearInterval(intervalId);
+  }
+  runTimer();
+}
+
+function timer3() {
+  number=3;
+  function runTimer() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+  function decrement() {
+    number--;
+    $("#timer").text(number);
+    if (number == 0) {
+      stopTimer();
+      $("#timer").empty();
+      setQuestion();
+    }
+  }
+  function stopTimer() {
+    clearInterval(intervalId);
+  }
+  runTimer();
+}
